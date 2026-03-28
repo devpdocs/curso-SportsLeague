0f4525c6604b2565f4c7ec7c9f7ae809de8e2d9d
devpdocs <moisestuberquia@gmail.com>
2026-03-28 14:43:24 -0500

    fase 02 finalizada

--- SportsLeague.API/Mappings/MappingProfile.cs
+++ SportsLeague.API/Mappings/MappingProfile.cs
@@ -22,6 +22,18 @@ public class MappingProfile : Profile
 
         CreateMap<Team, TeamResponseDTO>();
 
+        // Player mappings
+
+        CreateMap<PlayerRequestDTO, Player>();
+
+        CreateMap<Player, PlayerResponseDTO>()
+
+        .ForMember(
+
+        dest => dest.TeamName,
+
+        opt => opt.MapFrom(src => src.Team.Name));
+
     }
 
 }
--- SportsLeague.DataAccess/Migrations/20260328012019_AddPlayerEntity.Designer.cs
+++ SportsLeague.DataAccess/Migrations/20260328012019_AddPlayerEntity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportsLeague.DataAccess.Context;
 
@@ -10,9 +11,11 @@ using SportsLeague.DataAccess.Context;
 namespace SportsLeague.DataAccess.Migrations
 {
     [DbContext(typeof(LeagueDbContext))]
-    partial class LeagueDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260328012019_AddPlayerEntity")]
+    partial class AddPlayerEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.25");
