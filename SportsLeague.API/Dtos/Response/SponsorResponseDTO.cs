ba0ca3cdf2b317606e437b3b2e39a40c1b03bb28
devpdocs <moisestuberquia@gmail.com>
2026-04-03 19:01:33 -0500

    Retoques para desbugear

--- SportsLeague.API/Dtos/Response/SponsorResponseDTO.cs
+++ SportsLeague.API/Dtos/Response/SponsorResponseDTO.cs
@@ -7,6 +7,8 @@ public class SponsorResponseDTO
 {
     public int Id { get; set; }
 
+    public string Name { get; set; } = string.Empty;
+
     public string ContactEmail { get; set; } = string.Empty;
 
     public string? Phone { get; set; }
